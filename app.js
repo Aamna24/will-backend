b5cdfd00e0b6b9f2b972cf36700af577ae21e37a
Aamna Majid <aamna.majid@gmail.com>
2021-04-03 15:24:35 +0500

    preparing for deployment

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 const cors = require("cors");
+const config = require("config");
 const dotenv = require('dotenv')
 dotenv.config()
 
